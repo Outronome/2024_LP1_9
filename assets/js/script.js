1c93c70969f95dc1767bbf1ae77d2a6f5f9c41de
Rafael Ferraria <rafaelferraria@gmail.com>
2024-12-15 19:27:48 +0000

    Versão Final com correção de Erros e com relatório

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,30 @@
+// Ir para o topo 
 const scrollToTop = () => {
     window.scrollTo({
     top: 0,
     behavior: 'smooth'
     })
 }
-
-// function to set a given theme/color-scheme
+// Função que define o tema no localStorage e aplica na página
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
-
-// function to toggle between light and dark theme
+// Função que alterna entre o tema claro e escuro
 function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
-    }
+    const newTheme = localStorage.getItem('theme') === 'theme-dark' ? 'theme-light' : 'theme-dark';
+    setTheme(newTheme);
+    // Atualiza o checkbox com o estado do tema
+    document.getElementById('theme-checkbox').checked = (newTheme === 'theme-dark');
 }
-
-// Immediately invoked function to set the theme on initial load
-(function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
+// Função que é chamada ao carregar a página para aplicar o tema salvo
+document.addEventListener('DOMContentLoaded', function () {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'theme-dark') {
         setTheme('theme-dark');
-        document.getElementById('slider').checked = false;
+        document.getElementById('theme-checkbox').checked = true; // Marca o checkbox
     } else {
         setTheme('theme-light');
-      document.getElementById('slider').checked = true;
+        document.getElementById('theme-checkbox').checked = false; // Desmarca o checkbox
     }
-})();
+});
